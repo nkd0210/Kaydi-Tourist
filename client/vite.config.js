8b5e928765cc80e989b1f5fecc6b725200fa8e46
Nguyễn Kim Dũng <kimdungvn52@gmail.com>
2024-08-31 18:04:28 +0700

    test deploy FE

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        // target: "http://localhost:3000", //run on localhost
+        target: "https://kaydi-tourist-backend.vercel.app", // deploy on vercel
         secure: false,
       },
     },
